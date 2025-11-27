0581424ac9c1183ef54c0cce40b5997c5df8689b
sevcenconik07-afk <sevcenconik07@gmail.com>
2025-11-27 21:41:37 +0200

    Add files via upload

---
+++ LabC++1.1.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+
+void C() 
+{
+    std::cout << "called C" << std::endl;
+}
+void B()
+{
+    std::cout << "called B" << std::endl;
+}
+void A()
+{
+    std::cout << "called A" << std::endl;
+    B();
+    C();
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    A();
+}
+
+
